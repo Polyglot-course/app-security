defe855d03ea3a79da93ccb11856449310aa8dee
Mateo Loaiza Rios <mateo@nereus.io>
2025-02-22 12:20:46 -0500

    add nacos provider

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const appPromise = require('./src/app/middlewares/configprovider').appPromise;
 
-const PORT = process.env.SERVER_PORT_SECURITY || 3001
-
-app.use(express.json())
-app.use('/api', require('./src/app/routes'))
-
-app.listen(PORT, () => {
-    console.log('Application running on port ', PORT)
-})
-
-
+appPromise.then(function(app) {
+    const PORT = process.env.SERVER_PORT_SECURITY || 3001
+    app.use('/api', require('./src/app/routes'))
+    app.listen(PORT, () => {
+        console.log('Application running on port ', PORT)
+    })
+});
