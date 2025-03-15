1b71d82ed0daa332b0a0fc26b91612997b1b4542
Mateo Loaiza Rios <mateo@nereus.io>
2025-03-15 11:03:58 -0500

    changes in code, and pipeline implementation

---
+++ src/app/common/constants.js
@@ -0,0 +1,5 @@
+const APP_NAME = 'SecurityApp'
+ 
+module.exports = {
+    APP_NAME
+}
--- src/app/controllers/auth.controller.js
+++ src/app/controllers/auth.controller.js
@@ -1,15 +1,19 @@
+const logProvider = require('../middlewares/logprovider');
 const authService = require('../services/auth.service')
 const jwt = require('jsonwebtoken');
 
 const getUsersByCredentials = async (req, res) => {
+    logProvider.info('Get Users By Credentials');
     const { userName, password } = req.body
     const result = await authService.getUsersByCredentials(userName, password)
     if(result.length > 0)
     {
+        logProvider.info('Authorized user');
         const token = jwt.sign({ username: userName }, process.env.TOKEN_SECRET, { expiresIn: process.env.TOKEN_EXPIRATION })
         return res.status(200).json({ token: token, username: userName, expiration: process.env.TOKEN_EXPIRATION })
     }
     else
+        logProvider.error('Unauthorized user');
         return res.status(401).send({ error: 'Unauthorized user' });
 }
 
